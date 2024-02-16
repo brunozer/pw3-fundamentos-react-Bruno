2147f9e80024aaecae803699a80a0b54a3e8ecbe
bruno <bruno.costa218@etec.sp.gov.br>
2024-02-15 22:21:43 -0300

    books added and create component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import cavernas from './livros/cavernas_aco.jpg';
 import './App.css'; 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-      </header>
+
+      <div className='container'></div>
+      <h1>As cavernas de aço</h1>
+      <h3>Isaki asimovi</h3>
+
+      <img src={cavernas} className="App-logo" alt="capa livro as cavernas de-aço" />
+      <p>As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica.
+         A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário,
+          inclusive nos dramas policiais. A obra foi publicada, pela primeira vez, na revista Galaxy Science Fiction, e o sucesso foi tão grande que, um ano depois, se tornou livro.
+           Na trama, Os Mundos Siderais, antigas colônias da Terra, possuem rígidas leis de imigração, e não aceitam mais terráqueos em seus planetas. 
+           Para agravar a situação, os robôs estão substituindo grande parte da mão de obra trabalhadora na Terra, 
+           levando os humanos a perderem seus empregos e privilégios de cidadãos.</p>
     </div>
   );
 }
